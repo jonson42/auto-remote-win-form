6ab9dd0ead1e7f7c63c1e79e3d63da3aa8b84cb9
Nghiem Trinh Quoc <nghiemtq@unit.com.vn>
2021-06-20 17:45:54 +0700

    update code

--- auto-remote-win-form/AutoSeleniumFromExcel/Program.cs
+++ auto-remote-win-form/AutoSeleniumFromExcel/Program.cs
@@ -16,7 +16,7 @@ namespace AutoSeleniumFromExcel
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Login());
+            Application.Run(new Form1());
         }
     }
 }
